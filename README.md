d7dac3af883acebac7aa12e6350d8ba9d7adf85e
legal-s <legal-s@mgdis.fr>
2017-11-12 20:42:09 +0100

    feat(kendo): Ajout des composants "grid" et "datepicker"

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Vue.js
+# Vue.js + KendoUI
 
-Projet permettant d'expérimenter le framework Vue.js
+Projet permettant de mettre en oeuvre une application utilisant le framework Vue.js en combinaison avec "KendoUI".
+
+## Mise en oeuvre
+
+Initialisation du projet avec le template "webpack-simple" du CLI de Vue.js
+
+```
+vue init webpack-simple vuejs-kendoui
+```
+
---
+++ client/src/app.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+    <hello></hello>
+    <grid></grid>
+  </div>
+</template>
+
+<script>
+  // Composants
+  import Hello from './components/date-picker.vue';
+  import Grid from './components/grid.vue';
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: {
+      Hello,
+      Grid
+    }
+  }
+
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
